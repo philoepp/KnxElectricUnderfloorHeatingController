587fbb1e6c51cb202d4741db71e4a9d8422e926d
Philipp Niedermeier <philipp@niedermeier.me>
2022-01-05 16:17:28 +0100

    Added first test implementation of KNX support

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <KnxTpUart.h>
 
 /* -------------------------------------------------------------------------- 
 * DEFINES
 ---------------------------------------------------------------------------- */
+#define DEBUG
+
 #define TEMP_SENSOR_PIN         2     // IO pin of the used arduino
 #define TEMP_SENSOR_RESOLUTION  12    // [bit] Resolution in bit (9 to 12)
-#define TEMP_SENSOR_READ_CYCLE  3000  // [s] Time base for collecting new temperature values
+#define TEMP_SENSOR_READ_CYCLE  3000  // [ms] Time base for collecting new temperature values
+#define TEMP_SENSOR_SEND_CYCLE  30000 // [ms] Time base for sending the new temperature to the KNX bus
+
+#define TEMP_SETPOINT_MIN       6     // [°C]  6°C Frost protection
+#define TEMP_SETPOINT_MAX       35    // [°C] 35°C should be enough?..
+
+#define KNX_PA                  "1.0.181" // PA of the device
+#define KNX_GA_TEMP_CURRENT     "8/2/9"   // GA for the measured temperature
+#define KNX_GA_TEMP_SETPOINT    "8/2/10"   // GA for the temperature setpoint
 
 /* -------------------------------------------------------------------------- 
 Objects/variables
@@ -19,27 +30,38 @@ OneWire oneWire(TEMP_SENSOR_PIN);
 DallasTemperature sensors(&oneWire);
 DeviceAddress tempDeviceAddress;
 
+// KNX device
+KnxTpUart knx(&Serial1, KNX_PA);
+
 float temperature = 0.0;
+float temperatureSetpoint = 0.0;
 
 /* -------------------------------------------------------------------------- 
 * STATIC FUNCTION PROTOTYPES
 ---------------------------------------------------------------------------- */
 static void vTemperatureSensorSetup(void);
 static void vReadTemperatureFromSensor(void);
+static void vInitializeKNX(void);
+static void vSendTemperatureToKnx(void);
 
 /* -------------------------------------------------------------------------- 
 * FUNCTIONS
 ---------------------------------------------------------------------------- */
 void setup(void)
 {
+#ifdef DEBUG
   Serial.begin(9600);
+#endif
+
+  vInitializeKNX();
   vTemperatureSensorSetup();
 }
 
 void loop(void)
 { 
   vReadTemperatureFromSensor();
-  delay(10);
+  vSendTemperatureToKnx();
+  delay(1);
 }
 
 /* -------------------------------------------------------------------------- 
@@ -86,8 +108,89 @@ static void vReadTemperatureFromSensor(void)
     sensors.requestTemperatures(); 
     u32LastRequest = millis(); 
 
-    // Debug output
+#ifdef DEBUG
     Serial.print("Temperature: ");
     Serial.println(temperature);
+#endif
+  }
+}
+
+static void vSendTemperatureToKnx(void)
+{
+  static uint32_t u32LastTime = millis();
+
+  // Check if time period is over
+  if (millis() - u32LastTime > TEMP_SENSOR_SEND_CYCLE)
+  {
+    knx.groupWrite2ByteFloat(KNX_GA_TEMP_CURRENT, temperature);
+    u32LastTime = millis(); 
+  }
+}
+
+static void vInitializeKNX(void) 
+{
+  // Initialize connection to KNX BCU
+  Serial1.begin(19200,SERIAL_8E1); // Even parity;
+  while (!Serial1) {
+    ; // wait for serial port to connect
+  }
+
+  // Reset UART
+  if(Serial.available()) {
+    knx.uartReset();
+  }
+
+  // Register KNX group addresses
+  knx.addListenGroupAddress(KNX_GA_TEMP_CURRENT);
+  knx.addListenGroupAddress(KNX_GA_TEMP_SETPOINT); 
+}
+
+void serialEvent1() 
+{
+  KnxTpUartSerialEventType eType = knx.serialEvent();
+
+  if(eType == KNX_TELEGRAM) 
+  {
+    KnxTelegram* telegram = knx.getReceivedTelegram();
+
+    String target = String(0 + telegram->getTargetMainGroup())   + "/" +
+                    String(0 + telegram->getTargetMiddleGroup()) + "/" +
+                    String(0 + telegram->getTargetSubGroup());
+
+#ifdef DEBUG
+  Serial.print("KNX Event on: ");
+  Serial.println(target);
+#endif
+
+    // Is it a read request?
+    switch(telegram->getCommand())
+    {
+      case KNX_COMMAND_READ:
+        // Is the destination address equal to group address KNX_GA_TEMPERATURE
+        if(strcmp(target.c_str(), KNX_GA_TEMP_CURRENT) == 0) 
+        {
+          knx.groupAnswer2ByteFloat(KNX_GA_TEMP_CURRENT, temperature);
+        }
+        break;
+
+      case KNX_COMMAND_WRITE:
+        if (strcmp(target.c_str(), KNX_GA_TEMP_SETPOINT) == 0) 
+        {
+          temperatureSetpoint = telegram->get2ByteFloatValue();
+
+#ifdef DEBUG
+          Serial.print("Temperature setpoint: ");
+          Serial.println(temperatureSetpoint);
+#endif
+
+          // Limit temperature setpoint to useful values
+          temperatureSetpoint = min(temperatureSetpoint, TEMP_SETPOINT_MAX);
+          temperatureSetpoint = max(temperatureSetpoint, TEMP_SETPOINT_MIN); 
+        }  
+        break;
+
+      default:
+        break;
+    }
   }
 }
